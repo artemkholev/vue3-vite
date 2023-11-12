178e54b9c0dcabcaa8e20a1ccffeafe787884522
Artem <artemkholev@yandex.ru>
2023-11-12 22:30:02 +0300

    edit error with footer and app

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,69 @@
 <template>
   <div class="wrapper">
-    <PageHeader @incrCountEmit="(value) => console.log('emit', value)" :nameComponent="tesrVar"/>
+    <PageHeader @incrCountEmit="(value) => console.log('emit', value)" :nameComponent="testVar" />
     <PageContent>
-      <p>{{'name ' + user.name }}</p>
-      <p @click="incrAge">{{'age ' + user.age }}</p>
-      <p>{{'doubleAge ' + doubleAge }}</p>
+      <p>{{ 'name' + user.name }}</p>
+      <p @click="incrAge">{{ 'age' + user.age }}</p>
+      <p>{{ 'doubleAge' + doubleAge }}</p>
       <p>{{ emitData }}</p>
       <template #bottomSlot>
         <p>Bottom Slot Content</p>
       </template>
     </PageContent>
-    <PageFooter @footer-emit="(value) => {emitData = value, console.log('footerEmit ', value)}" 
+    <PageFooter
       :user="user"
-      :
-      />
+      :incrAge="incrAge"
+      @footer-emit="
+        (value) => {
+          console.log('footerEmit');
+          emitData = value;
+        }
+      "
+    />
   </div>
 </template>
 
-
 <script setup lang="ts">
-import PageContent from '@/components/PageContent.vue';
-import PageFooter from './components/PageFooter/PageFooter.vue';
 import PageHeader from '@/components/PageHeader.vue';
-import { ref, computed, watch } from 'vue';
-import { type IUser } from './views/App.types';
+import PageContent from '@/components/PageContent.vue';
+import PageFooter from '@/components/PageFooter/PageFooter.vue';
+import { computed, ref, watch } from 'vue';
+import type { IUser } from './views/App.types';
 
-const tesrVar = ref('HeaderTest');
+//переменные реактивные
+const testVar = ref('HeaderTest');
 
 const user = ref<IUser>({
   name: 'Serg',
   age: 38
-})
+});
 
-const emitData = 
+const emitData = ref<string>('');
 
+//методы
 const incrAge = () => {
   user.value.age++;
-}
+};
 
-//computed 
+//computed
+
 const doubleAge = computed(() => {
   return user.value.age * 2;
-})
+});
 
-watch(user,
+//вотчеры
+
+watch(
+  user,
   (newUser, oldUser) => {
-    console.log(newUser.age + " " + oldUser.age)
+    // из официальной документации
+    // Note: `newValue` will be equal to `oldValue` here
+    // *unless* state.someObject has been replaced
+    console.log('newUser ', newUser.age);
+    console.log('oldUser ', oldUser.age);
   },
-  { deep: true })
+  { deep: true }
+);
 </script>
 
 <style lang="scss" scoped>
@@ -57,7 +73,6 @@ watch(user,
   justify-content: space-between;
   min-height: 100vh;
 }
-
 .content {
   flex-grow: 1;
 }
--- src/components/PageFooter/PageFooter.vue
+++ src/components/PageFooter/PageFooter.vue
@@ -1,26 +1,31 @@
 <template>
   <footer @click="emits('footerEmit', 'my-footer')" class="footer">
-    Footer
+    {{ user }}
+    <button @click="incrAge">incrAgeFooter</button>
   </footer>
 </template>
 
 <script setup lang="ts">
-import { toRefs } from "vue";
-import type { IPropsFooter, IFooterEmits } from "./PageFooter.types"
+import { toRefs } from 'vue';
+import type { IFooterEmits, IPropsFooter } from './PageFooter.types';
 
 const props = withDefaults(defineProps<IPropsFooter>(), {
   user: () => {
-    return {user, age}
+    return { name: 'DefaultUserName', age: 10 };
   }
-}) ;
+});
 
 const { user } = toRefs(props);
 
 const emits = defineEmits<IFooterEmits>();
 
 console.log(props.user);
-
 </script>
 
 <style src="./PageFooter.style.scss" lang="scss" scoped></style>
+<style lang="scss" scoped>
+.footer {
+  color: green;
+}
+</style>
 
